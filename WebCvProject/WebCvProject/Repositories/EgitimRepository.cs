cf1a960ce5215e964f3cdea6e14c13ecc297895e
= <omerfarukkpala@gmail.com>
2024-01-08 15:52:09 +0300

    ..

---
+++ WebCvProject/WebCvProject/Repositories/EgitimRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebCvProject.Repositories
+{
+    public class EgitimRepository
+    {
+    }
+}
