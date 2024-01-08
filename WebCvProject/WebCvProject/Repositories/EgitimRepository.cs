3a2091047975d33971ec00f8cd6de19ca316048b
= <omerfarukkpala@gmail.com>
2024-01-08 15:52:29 +0300

    : GenericRepository<TblEgitimlerim>

--- WebCvProject/WebCvProject/Repositories/EgitimRepository.cs
+++ WebCvProject/WebCvProject/Repositories/EgitimRepository.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebCvProject.Models.Entity;
 
 namespace WebCvProject.Repositories
 {
-    public class EgitimRepository
+    public class EgitimRepository : GenericRepository<TblEgitimlerim>
     {
     }
 }
