a2099ab879e01349675b65e01fba7cb05646a316
= <omerfarukkpala@gmail.com>
2024-01-08 15:54:02 +0300

    : GenericRepository<TblHakkimda>

--- WebCvProject/WebCvProject/Repositories/HakkimdaRepository.cs
+++ WebCvProject/WebCvProject/Repositories/HakkimdaRepository.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebCvProject.Models.Entity;
 
 namespace WebCvProject.Repositories
 {
-    public class HakkimdaRepository
+    public class HakkimdaRepository : GenericRepository<TblHakkimda>
     {
     }
 }
