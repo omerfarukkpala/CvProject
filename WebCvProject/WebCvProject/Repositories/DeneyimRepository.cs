092dd7aa0bc3e4f33c2d853aab9270950d69bf8a
= <omerfarukkpala@gmail.com>
2024-01-08 15:36:21 +0300

    ..

--- WebCvProject/WebCvProject/Repositories/DeneyimRepository.cs
+++ WebCvProject/WebCvProject/Repositories/DeneyimRepository.cs
@@ -2,14 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using WebCvProject.Models.Entity;
 
 namespace WebCvProject.Repositories
 {
-    public class GenericRepository
+    public class DeneyimRepository
     {
-
-
 
     }
 }
