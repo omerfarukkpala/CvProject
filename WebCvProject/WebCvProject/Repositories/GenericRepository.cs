54ff92194ccc94975c76fc2e37ce8df9de915f8f
= <omerfarukkpala@gmail.com>
2024-01-08 15:37:15 +0300

    ..

--- WebCvProject/WebCvProject/Repositories/GenericRepository.cs
+++ WebCvProject/WebCvProject/Repositories/GenericRepository.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.Ajax.Utilities;
 using System.Web;
 using WebCvProject.Models.Entity;
 
 namespace WebCvProject.Repositories
 {
-    public class GenericRepository
+    public class GenericRepository<T> where T : class, new()
     {
 
 
