683243ed851ee5b6a0e59f8858b5ee703765e6a2
= <omerfarukkpala@gmail.com>
2024-01-08 15:57:02 +0300

    : GenericRepository<TblIletisim>

--- WebCvProject/WebCvProject/Repositories/IletisimRepository.cs
+++ WebCvProject/WebCvProject/Repositories/IletisimRepository.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebCvProject.Models.Entity;
 
 namespace WebCvProject.Repositories
 {
-    public class IletisimRepository
+    public class IletisimRepository : GenericRepository<TblIletisim>
     {
     }
 }
