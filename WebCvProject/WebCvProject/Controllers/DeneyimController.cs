9efae3c942e6dd823275ff631cba633ab855a019
= <omerfarukkpala@gmail.com>
2024-01-08 16:12:10 +0300

    ...

--- WebCvProject/WebCvProject/Controllers/DeneyimController.cs
+++ WebCvProject/WebCvProject/Controllers/DeneyimController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebCvProject.Repositories;
 
 namespace WebCvProject.Controllers
 {
     public class DeneyimController : Controller
     {
         // GET: Deneyim
+        DeneyimRepository repo = new DeneyimRepository();
 
         public ActionResult Index()
         {
-            return View();
+            var degerler = repo.List();
+            return View(degerler);
         }
     }
 }
