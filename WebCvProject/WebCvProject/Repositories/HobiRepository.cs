aab701faafdb1ee720a73501420da3d0ca87229a
= <omerfarukkpala@gmail.com>
2024-01-08 16:00:10 +0300

    : GenericRepository<TblHobilerim>

--- WebCvProject/WebCvProject/Repositories/HobiRepository.cs
+++ WebCvProject/WebCvProject/Repositories/HobiRepository.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebCvProject.Models.Entity;
 
 namespace WebCvProject.Repositories
 {
-    public class HobiRepository 
+    public class HobiRepository : GenericRepository<TblHobilerim>
     {
     }
 }
