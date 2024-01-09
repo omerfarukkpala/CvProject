7390fb5355ac8be30419deae60859f9901b6cbc6
= <omerfarukkpala@gmail.com>
2024-01-09 10:51:59 +0300

    ..

--- WebCvProject/WebCvProject/Controllers/YetenekController.cs
+++ WebCvProject/WebCvProject/Controllers/YetenekController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebCvProject.Models.Entity;
+using WebCvProject.Repositories;
 
 namespace WebCvProject.Controllers
 {
     public class YetenekController : Controller
     {
-        // GET: Yetenek
+        //DbCvEntities db = new DbCvEntities();
+        GenericRepository<TblYeteneklerim> repo = new GenericRepository<TblYeteneklerim>();
         public ActionResult Index()
         {
-            return View();
+            var yetenekler = repo.List();
+            return View(yetenekler);
         }
     }
 }
