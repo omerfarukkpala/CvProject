effbc0e721edb2920599d05034336a622adf2276
= <omerfarukkpala@gmail.com>
2024-01-09 16:43:12 +0300

    ..

--- WebCvProject/WebCvProject/Controllers/EgitimController.cs
+++ WebCvProject/WebCvProject/Controllers/EgitimController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebCvProject.Models.Entity;
+using WebCvProject.Repositories;
 
 namespace WebCvProject.Controllers
 {
+    [Authorize]
     public class EgitimController : Controller
     {
         // GET: Egitim
+        GenericRepository<TblEgitimlerim> repo = new GenericRepository<TblEgitimlerim>();
+
         public ActionResult Index()
         {
-            return View();
+            var egitim = repo.List();
+            return View(egitim);
         }
+        [HttpGet]
+
     }
 }
