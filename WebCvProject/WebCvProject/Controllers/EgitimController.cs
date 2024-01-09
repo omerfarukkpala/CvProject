83eb4acd77fa9bfd440dfccc5f8f78e18365adbb
= <omerfarukkpala@gmail.com>
2024-01-09 16:41:24 +0300

    ..

---
+++ WebCvProject/WebCvProject/Controllers/EgitimController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCvProject.Controllers
+{
+    public class EgitimController : Controller
+    {
+        // GET: Egitim
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
