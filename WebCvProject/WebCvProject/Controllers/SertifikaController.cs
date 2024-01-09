ea8505a326d74befa952bb83ed79ceb7d79bb355
= <omerfarukkpala@gmail.com>
2024-01-09 18:03:40 +0300

    ...

--- WebCvProject/WebCvProject/Controllers/SertifikaController.cs
+++ WebCvProject/WebCvProject/Controllers/SertifikaController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebCvProject.Models.Entity;
+using WebCvProject.Repositories;
 
 namespace WebCvProject.Controllers
 {
     public class SertifikaController : Controller
     {
         // GET: Sertifika
+        GenericRepository<TblSertifikalarim> repo = new GenericRepository<TblSertifikalarim>();
+        DbCvEntities db = new DbCvEntities();
         public ActionResult Index()
         {
-            return View();
+            var sertifika = repo.List();
+            return View(sertifika);
         }
     }
 }
