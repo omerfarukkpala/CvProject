b50c3fa4ccd6fbb23d5c57f5bb91d05b967ee697
= <omerfarukkpala@gmail.com>
2024-01-10 13:48:07 +0300

    ..

--- WebCvProject/WebCvProject/Controllers/LoginController.cs
+++ WebCvProject/WebCvProject/Controllers/LoginController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using WebCvProject.Models.Entity;
 
 namespace WebCvProject.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(TblAdmin p)
+        {
+            DbCvEntities db = new DbCvEntities();
+            var bilgi = db.TblAdmin.FirstOrDefault(x => x.KullaniciAdi == p.KullaniciAdi && x.Sifre == p.Sifre);
+            if (bilgi != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgi.KullaniciAdi, false);
+                Session["KullaniciAdi"] = bilgi.KullaniciAdi.ToString();
+                return RedirectToAction("Index", "Deneyim");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
         }
     }
 }
