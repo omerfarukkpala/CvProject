2bd772fef2d3827b0bf64c852dceede9250f150e
= <omerfarukkpala@gmail.com>
2024-01-10 11:57:20 +0300

    ..

--- WebCvProject/WebCvProject/Controllers/SosyalMedyaController.cs
+++ WebCvProject/WebCvProject/Controllers/SosyalMedyaController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebCvProject.Models.Entity;
+using WebCvProject.Repositories;
 
 namespace WebCvProject.Controllers
 {
     public class SosyalMedyaController : Controller
     {
         // GET: SosyalMedya
+        GenericRepository<TblSosyalMedya> repo = new GenericRepository<TblSosyalMedya>();
+
         public ActionResult Index()
         {
-            return View();
+            var veriler = repo.List();
+            return View(veriler);
         }
     }
 }
