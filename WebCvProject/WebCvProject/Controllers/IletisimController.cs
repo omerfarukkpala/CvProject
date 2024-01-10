38f27d8555eeb8e5b27e0026ca1536ab6255c771
= <omerfarukkpala@gmail.com>
2024-01-10 10:46:11 +0300

    ..

--- WebCvProject/WebCvProject/Controllers/IletisimController.cs
+++ WebCvProject/WebCvProject/Controllers/IletisimController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebCvProject.Models.Entity;
+using WebCvProject.Repositories;
 
 namespace WebCvProject.Controllers
 {
     public class IletisimController : Controller
     {
         // GET: Iletisim
+        GenericRepository<TblIletisim> repo = new GenericRepository<TblIletisim>();
         public ActionResult Index()
         {
             return View();
