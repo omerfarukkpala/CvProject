61edd23583ee334a06c5bc510f149c2328526025
= <omerfarukkpala@gmail.com>
2024-01-10 11:03:49 +0300

    ..

---
+++ WebCvProject/WebCvProject/Controllers/HobiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebCvProject.Models.Entity;
+using WebCvProject.Repositories;
+
+namespace WebCvProject.Controllers
+{
+    public class HobiController : Controller
+    {
+        // GET: Hobi
+        GenericRepository<TblHobilerim> repo = new GenericRepository<TblHobilerim>();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
