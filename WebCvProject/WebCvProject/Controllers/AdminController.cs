cfea0bc279c5362482765bf4e6b1d9a877904eae
= <omerfarukkpala@gmail.com>
2024-01-10 14:58:36 +0300

    ..

--- WebCvProject/WebCvProject/Controllers/AdminController.cs
+++ WebCvProject/WebCvProject/Controllers/AdminController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebCvProject.Models.Entity;
+using WebCvProject.Repositories;
 
 namespace WebCvProject.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
+        GenericRepository<TblAdmin> repo = new GenericRepository<TblAdmin>();
+
         public ActionResult Index()
         {
-            return View();
+            var liste = repo.List();
+            return View(liste);
         }
     }
 }
