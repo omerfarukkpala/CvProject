f2a79c6dec14b4f452978bf309aefc8e376ec53d
= <omerfarukkpala@gmail.com>
2024-01-10 11:43:46 +0300

    ...

---
+++ WebCvProject/WebCvProject/Controllers/SosyalMedyaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCvProject.Controllers
+{
+    public class SosyalMedyaController : Controller
+    {
+        // GET: SosyalMedya
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
